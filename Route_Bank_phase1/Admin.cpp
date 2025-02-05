0c2d1a89e0f430b33c939ca20ab1603e13b48010
Mohamed Tarek <129046548+Donatello7@users.noreply.github.com>
2025-02-05 19:50:07 +0200

    Update Admin.cpp

--- Route_Bank_phase1/Admin.cpp
+++ Route_Bank_phase1/Admin.cpp
@@ -3,32 +3,29 @@
 using namespace std;
 
 void Admin::addEmployee(Employee& employee) {
-    cout << "Added new employee: " << employee.getName() << endl;
+    
 }
 
 Employee* Admin::searchEmployee(int id) {
-    // Placeholder function, implement search logic as required
-    return nullptr;
+    
 }
 
 void Admin::editEmployee(int id, const string& name, const string& password, double salary) {
-    // Placeholder function, implement editing logic as required
-    cout << "Editing employee with ID " << id << endl;
+    
 }
 
 void Admin::listEmployee() {
-    // Placeholder function, implement listing logic as required
-    cout << "Listing all employees (currently empty)." << endl;
+    
 }
 
 void Admin::addClient(Client& client) {
-    cout << "Admin added client: " << client.getName() << endl;
+    
 }
 
 void Admin::editClient(int id, const string& name, const string& password, double balance) {
-    cout << "Admin edited client with ID " << id << endl;
+    
 }
 
 void Admin::listClient() {
-    cout << "Listing all clients (currently empty)." << endl;
+    
 }
