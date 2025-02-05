4a5b0ba447e14b66fa6ac9107690be21d8031198
Mohamed Tarek <129046548+Donatello7@users.noreply.github.com>
2025-02-05 22:09:39 +0200

    Update Validation.h

--- Route_Bank_phase1/Validation.h
+++ Route_Bank_phase1/Validation.h
@@ -2,11 +2,12 @@
 #define VALIDATION_H
 
 #include <string>
+using namespace std;
 
 class Validation {
 public:
-    static bool isValidName(const std::string& name);
-    static bool isValidPassword(const std::string& password);
+    static bool isValidName(const string& name);
+    static bool isValidPassword(const string& password);
     static bool isValidBalance(double balance);
     static bool isValidSalary(double salary);
 };
